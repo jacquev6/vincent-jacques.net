6c4051e0b9592c00a710ba97bbd084fced720689
Vincent Jacques <vincent@vincent-jacques.net>
2018-08-18 18:12:11 +0200

    Minify generated website

--- run_development_cycle.sh
+++ run_development_cycle.sh
@@ -6,6 +6,8 @@ PROJECT_ROOT=$(pwd)
 
 JEKYLL_COMMAND=build
 
+SERVE_PROD=false
+
 SHOW_IN_BROWSER=false
 function show_in_browser {
   echo
@@ -23,8 +25,11 @@ do
     -wb|--web-browser)
       SHOW_IN_BROWSER=true
       ;;
-    -s|--serve)
+    -s|--serve-dev)
       JEKYLL_COMMAND="serve --livereload"
+      ;;
+    -p|--serve-prod)
+      SERVE_PROD=true
       ;;
     *)
       echo "Unknown parameter passed: $1"
@@ -34,7 +39,33 @@ do
 done
 
 
+npm install
+
 jekyll $JEKYLL_COMMAND --source jekyll-src --destination docs
+
+for f in $(find docs -name "*.html")
+do
+  ./node_modules/.bin/html-minifier \
+    --collapse-boolean-attributes \
+    --collapse-whitespace \
+    --decode-entities \
+    --minify-css true \
+    --minify-js true \
+    --remove-attribute-quotes \
+    --remove-comments \
+    --remove-script-type-attributes \
+    --remove-style-link-type-attributes \
+    --remove-tag-whitespace \
+    --sort-attributes \
+    --sort-class-name \
+    --use-short-doctype \
+    $f -o $f
+done
+
+if $SERVE_PROD
+then
+  (cd docs; python3 -m http.server)
+fi
 
 
 echo

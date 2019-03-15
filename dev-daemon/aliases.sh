0132e84702d239820bdb8f5025b947787e7f408a
Vincent Jacques <vincent@vincent-jacques.net>
2019-03-15 11:36:42 +0100

    Homogenize dev-daemon with other projects

--- dev-daemon/aliases.sh
+++ dev-daemon/aliases.sh
@@ -1,10 +1,13 @@
 # To be sourced as '. dev-daemon/aliases.sh'
 
+PROJECT_ROOT=$PWD
+
 function dev-daemon-docker-compose {
-  DEV_DAEMON_UID=$(id -u) DEV_DAEMON_GID=$(id -g) docker-compose --project-name dev-daemon-vjnet --file dev-daemon/docker-compose.yml "$@"
+  DEV_DAEMON_UID=$(id -u) DEV_DAEMON_GID=$(id -g) docker-compose --project-name dev-daemon-$(basename $PROJECT_ROOT) --file $PROJECT_ROOT/dev-daemon/docker-compose.yml "$@"
 }
 
-dev-daemon-docker-compose up --build -d
+# -d because --detach is unavailable on Ubuntu (docker-compose version 1.17.1)
+dev-daemon-docker-compose up --build -d dev-daemon
 
 function dev-daemon-exec {
   dev-daemon-docker-compose exec --user $(id -u):$(id -g) dev-daemon "$@"

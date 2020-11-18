0ec5d46331265e3bc20551f90ac857479191d086
Vincent Jacques <vincent@vincent-jacques.net>
2020-11-18 17:16:55 +0100

    Initiate portfolio

---
+++ pages/portfolio/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>
+      Portfolio
+    </h1>
+    <p>
+      For the moment, I have only <NuxtLink to="/portfolio/software">
+        software projects
+      </NuxtLink> to show.
+    </p>
+  </div>
+</template>
---
+++ pages/portfolio/software/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>
+      Open-source software projects
+    </h1>
+  </div>
+</template>

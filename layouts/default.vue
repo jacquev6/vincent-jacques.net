5f7a80787b3428e5e97f0637e1081ad3bfc38a94
Vincent Jacques <vincent@vincent-jacques.net>
2020-11-18 19:08:33 +0100

    Restore page header
    ... and some other details

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,21 +4,30 @@
       <b-col>
         <b-row>
           <b-col>
-            <p id="vjnet">
-              <NuxtLink to="/">
-                vincent-jacques.net
-              </NuxtLink>
-            </p>
+            <vj-markdown>
+              [vincent-jacques.net](/) {#vjnet}
+            </vj-markdown>
           </b-col>
         </b-row>
         <b-row>
-          <b-col lang="fr">
-            <p id="citation">
-              &laquo;&nbsp;S'il n'y a pas de solution, c'est qu'il n'y a pas de problème.&nbsp;&raquo;
-            </p>
+          <b-col cols="7" md="5" lg="6">
+            <vj-markdown>
+              &laquo;&nbsp;S'il n'y a pas de solution, c'est qu'il n'y a pas de problème.&nbsp;&raquo; {#citation}
 
-            <p id="citation-origin">
-              Devise <a href="https://en.wikipedia.org/wiki/Les_Shadoks">Shadock</a>
+              Devise [Shadock] {#citation-origin}
+
+              [Shadock]: https://en.wikipedia.org/wiki/Les_Shadoks
+            </vj-markdown>
+          </b-col>
+          <b-col cols="7" md="4">
+            <p class="contact">
+              <a href="mailto:vincent@vincent-jacques.net">e-mail <fa-icon :icon="['far', 'envelope']" /></a>
+              <a href="https://github.com/jacquev6/">GitHub <fa-icon :icon="['fab', 'github']" /></a>
+              <a href="https://travis-ci.org/jacquev6/">Travis CI <img height="15px" src="/travis.svg"></img></a>
+              <a href="https://pypi.org/user/jacquev6/">PyPi <fa-icon :icon="['fab', 'python']" /></a>
+              <a href="https://github.com/ocaml/opam-repository/search?q=author%3Ajacquev6&amp;type=Issues">OPAM <img height="15px" src="/ocaml.svg"></img></a>
+              <a href="https://www.linkedin.com/in/jacquev6/">LinkedIn <fa-icon :icon="['fab', 'linkedin']" /></a>
+              <a href="https://stackoverflow.com/users/905845/jacquev6/">StackOverflow <fa-icon :icon="['fab', 'stack-overflow']" /></a>
             </p>
           </b-col>
         </b-row>
--- nuxt.config.js
+++ nuxt.config.js
@@ -11,13 +11,11 @@ export default {
     title: 'vincent-jacques.net',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' }
     ],
     link: [
       { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.7.2/css/brands.css' },
       { rel: 'stylesheet', href: 'https://use.fontawesome.com/releases/v5.7.2/css/fontawesome.css' }
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
 

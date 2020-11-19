2ddee2801f2c0597176736ee522ed6ea0cddbf22
Vincent Jacques <vincent@vincent-jacques.net>
2020-11-19 17:05:58 +0100

    Build for GitHub Pages

--- docs/_nuxt/11ee59e.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{188:function(n,t,e){"use strict";var c=e(63),component=Object(c.a)({},(function(){var n=this.$createElement,t=this._self._c||n;return t("div",[t("Nuxt")],1)}),[],!1,null,null,null);t.a=component.exports},190:function(n,t,e){n.exports=e(191)}},[[190,3,1,4]]]);

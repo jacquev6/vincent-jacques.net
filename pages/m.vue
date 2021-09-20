8636fbef9a91aa7b862f47dd0aa182c546f68292
Vincent Jacques <vincent@vincent-jacques.net>
2021-09-20 10:07:17 +0200

    Make ESLint happy

--- pages/m.vue
+++ pages/m.vue
@@ -4,13 +4,13 @@
     <p>Maybe I can help you?</p>
     <p>Mentoring/tutoring in small groups</p>
     <p>All things related to software engineering</p>
-    <img src="https://chart.googleapis.com/chart?cht=qr&chl=https%3A%2F%2Fvincent-jacques.net%2Fmentor&chs=250x250&choe=UTF-8&chld=L|2" />
+    <img src="https://chart.googleapis.com/chart?cht=qr&chl=https%3A%2F%2Fvincent-jacques.net%2Fmentor&chs=250x250&choe=UTF-8&chld=L|2">
     <p><a href="https://vincent-jacques.net/mentor">https://vincent-jacques.net/mentor</a></p>
   </div>
 </template>
 
 <script>
-  export default {
-    layout: 'm'
-  }
+export default {
+  layout: 'm'
+}
 </script>
--- pages/mentor.vue
+++ pages/mentor.vue
@@ -9,17 +9,17 @@
           I'm offering my mentorship to software development students at all levels.
 
           This can be interesting for students in two situations:
-            - your school is going too fast, you feel lost and you need help to catch up
-            - your school is going too slow, you're bored and you'd like to talk about more advanced stuff
+          - your school is going too fast, you feel lost and you need help to catch up
+          - your school is going too slow, you're bored and you'd like to talk about more advanced stuff
 
           What I can help you with:
-            - understand everything I know about software development engineering (and I honestly know quite a lot)
-            - understand the big picture of how computers and the internet work
+          - understand everything I know about software development engineering (and I honestly know quite a lot)
+          - understand the big picture of how computers and the internet work
 
           What I *cannot* help you with:
-            - influence your grades (even if I'm teaching at your school)
-            - do your homework
-            - help you find a job
+          - influence your grades (even if I'm teaching at your school)
+          - do your homework
+          - help you find a job
 
           I think a deep understanding of the fundamentals is key to learning new things efficiently,
           so I will try and explain the fundamentals you need, *before* going into the details.
@@ -40,9 +40,9 @@
           This is a no-commitment offer: you and I can stop whenever one of us wants, for whatever reason.
 
           If you want me to continue mentoring you, you should:
-            - show up when we agree
-            - ask questions, suggest topics
-            - tell me how I'm doing, and what I can do to help you better
+          - show up when we agree
+          - ask questions, suggest topics
+          - tell me how I'm doing, and what I can do to help you better
 
           Please note that even if I'm teaching at your school, this is an entirely extra-curricular offer,
           completely unrelated with your school.
@@ -56,10 +56,10 @@
         </vj-hdr>
         <vj-markdown>
           Send me an email to [vincent.jacques@centraliens.net](mailto:vincent.jacques@centraliens.net?Subject=Mentoring) where you tell me:
-            - if you prefer to speak French or English
-            - where and what you study, at what level, etc. If I'm teaching one of your classes, tell me which one, in what group your are, etc.
-            - if you feel school is going too slow or too fast
-            - what you expect from this mentoring (in a few sentences)
+          - if you prefer to speak French or English
+          - where and what you study, at what level, etc. If I'm teaching one of your classes, tell me which one, in what group your are, etc.
+          - if you feel school is going too slow or too fast
+          - what you expect from this mentoring (in a few sentences)
         </vj-markdown>
       </b-col>
     </b-row>
